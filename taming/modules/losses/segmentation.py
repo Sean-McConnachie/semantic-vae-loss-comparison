7e6a6eb5af51d9607cc4c23c8e5eb15f53bb9da5
Sean McConnachie <seanjulian.mcc@gmail.com>
2025-09-10 11:37:36 +1200

    Added focal BCE loss for segmentation

--- taming/modules/losses/segmentation.py
+++ taming/modules/losses/segmentation.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -20,3 +21,25 @@ class BCELossWithQuant(nn.Module):
                       "{}/bce_loss".format(split): bce_loss.detach().mean(),
                       "{}/quant_loss".format(split): qloss.detach().mean()
                       }
+
+
+class FocalBCELossWithQuant(nn.Module):
+    def __init__(self, codebook_weight=1., gamma=2.0, alpha=1.0):
+        super().__init__()
+        self.codebook_weight = codebook_weight
+        self.gamma = gamma
+        self.alpha = alpha
+
+    def forward(self, qloss, target, prediction, split):
+        bce_loss = F.binary_cross_entropy_with_logits(prediction, target, reduction='none')
+        prob = torch.sigmoid(prediction)
+        pt = prob * target + (1 - prob) * (1 - target)
+        focal_weight = self.alpha * (1 - pt) ** self.gamma
+        focal_bce = (focal_weight * bce_loss).mean()
+
+        loss = focal_bce + self.codebook_weight * qloss
+        return loss, {
+            "{}/total_loss".format(split): loss.clone().detach().mean(),
+            "{}/focal_bce_loss".format(split): focal_bce.detach().mean(),
+            "{}/quant_loss".format(split): qloss.detach().mean()
+        }
