079d67b1d1e7b3c2a146e1421e2d5cf26189ebf1
Sean McConnachie <seanjulian.mcc@gmail.com>
2025-09-25 13:59:38 +1200

    added pixel-level class distribution script. loading is very slow, will look at converting cocostuffthings to wds

--- main.py
+++ main.py
@@ -147,12 +147,13 @@ class WrappedDataset(Dataset):
 
 class DataModuleFromConfig(pl.LightningDataModule):
     def __init__(self, batch_size, train=None, validation=None, test=None,
-                 wrap=False, num_workers=None, pin_memory=False):
+                 wrap=False, num_workers=None, pin_memory=False, persistent_workers=False):
         super().__init__()
         self.batch_size = batch_size
         self.dataset_configs = dict()
         self.num_workers = num_workers if num_workers is not None else batch_size*2
         self.pin_memory = pin_memory
+        self.persistent_workers = persistent_workers
         if train is not None:
             self.dataset_configs["train"] = train
             self.train_dataloader = self._train_dataloader
@@ -177,17 +178,17 @@ class DataModuleFromConfig(pl.LightningDataModule):
                 self.datasets[k] = WrappedDataset(self.datasets[k])
 
     def _train_dataloader(self):
-        return DataLoader(self.datasets["train"],
+        return DataLoader(self.datasets["train"], persistent_workers=self.persistent_workers,
                           batch_size=self.batch_size, pin_memory=self.pin_memory,
                           num_workers=self.num_workers, shuffle=True, collate_fn=custom_collate)
 
     def _val_dataloader(self):
-        return DataLoader(self.datasets["validation"],
+        return DataLoader(self.datasets["validation"], persistent_workers=self.persistent_workers,
                           batch_size=self.batch_size, pin_memory=self.pin_memory,
                           num_workers=self.num_workers, collate_fn=custom_collate)
 
     def _test_dataloader(self):
-        return DataLoader(self.datasets["test"],
+        return DataLoader(self.datasets["test"], persistent_workers=self.persistent_workers,
                           batch_size=self.batch_size, pin_memory=self.pin_memory,
                           num_workers=self.num_workers, collate_fn=custom_collate)
 
