ee525c03fae7a3ed16a5ecbade407154303b062b
Sean McConnachie <seanjulian.mcc@gmail.com>
2025-09-25 12:09:58 +1200

    Config fixes and training setup for server

--- run_training.sh
+++ run_training.sh
@@ -2,16 +2,17 @@
 
 # ======== SLURM Job Configuration ========
 #SBATCH --job-name=vqgan-seg
-#SBATCH --time=01:00:00                   # [REQUIRED] Wall time limit in HH:MM:SS
+#SBATCH --time=10:00:00                   # [REQUIRED] Wall time limit in HH:MM:SS
 #SBATCH --open-mode=append
-#SBATCH --output=/data/smcc417/output.log
-#SBATCH --error=/data/smcc417/error.log
+#SBATCH --output=/data/smcc417/taming-transformers/runs/output.log
+#SBATCH --error=/data/smcc417/taming-transformers/runs/error.log
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=10
 
 # ======== Job Execution Steps ========
 
 # Navigate to the working directory where your code and virtual environment are located
+source /home/smcc417/.bashrc
 cd /data/smcc417/taming-transformers
 conda activate taming
 python main.py --base configs/coco_cond_stage.yaml --gpus 0, -t True
