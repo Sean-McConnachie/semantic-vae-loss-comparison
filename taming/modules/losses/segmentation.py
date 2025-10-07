232dad844ac1a38829b104c25a7a465a0f80ffcf
Sean McConnachie <seanjulian.mcc@gmail.com>
2025-10-08 11:17:34 +1300

    added cross entropy + lovasz softmax loss

--- taming/modules/losses/segmentation.py
+++ taming/modules/losses/segmentation.py
@@ -1,6 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
+try:
+    from itertools import  ifilterfalse
+except ImportError: # py3k
+    from itertools import  filterfalse as ifilterfalse
+
 
 from main import get_obj_from_str
 from taming.modules.losses.vqperceptual import vanilla_d_loss, hinge_d_loss, adopt_weight
@@ -11,6 +17,7 @@ class BCELoss(nn.Module):
     def forward(self, prediction, target):
         loss = F.binary_cross_entropy_with_logits(prediction,target)
         return loss, {}
+
 
 class BCELossWithQuant(nn.Module):
     def __init__(self, codebook_weight=1.0):
@@ -27,6 +34,7 @@ class BCELossWithQuant(nn.Module):
         }
         return loss, log_dict
 
+
 class CELossWithQuant(nn.Module):
     def __init__(self, codebook_weight=1.0):
         super().__init__()
@@ -42,6 +50,7 @@ class CELossWithQuant(nn.Module):
             f"{split}/quant_loss": qloss.detach().mean()
         }
         return loss, log_dict
+
 
 class CEDiceLossWithQuant(nn.Module):
     def __init__(self, ce_weight=1.0, dice_weight=1.0, codebook_weight=1.0, num_classes=None, smooth=1e-6):
@@ -90,6 +99,7 @@ class CEDiceLossWithQuant(nn.Module):
 
         return loss, log_dict
 
+
 class FocalLossWithQuant(nn.Module):
     def __init__(self, codebook_weight=1.0, gamma=2.0, alpha=None):
         super().__init__()
@@ -120,6 +130,141 @@ class FocalLossWithQuant(nn.Module):
             f"{split}/quant_loss": qloss.detach().mean()
         }
         return loss, log_dict
+
+
+class CELovaszSoftmaxWithQuant(nn.Module):
+    def __init__(self, codebook_weight=1.0, lovasz_weight=1.0, ce_weight=1.0):
+        super().__init__()
+        self.codebook_weight = codebook_weight
+        self.lovasz_weight = lovasz_weight
+        self.ce_weight = ce_weight
+
+    def forward(self, qloss, target, prediction, split):
+        target_indices = torch.argmax(target, dim=1)
+        ce_loss = F.cross_entropy(prediction, target_indices)
+
+        probs = F.softmax(prediction, dim=1)
+        lovasz_loss = CELovaszSoftmaxWithQuant.lovasz_softmax(probs, target_indices, per_image=False, ignore=None)
+
+        loss = (self.ce_weight * ce_loss) + (self.lovasz_weight * lovasz_loss) + (self.codebook_weight * qloss)
+
+        log_dict = {
+            f"{split}/total_loss": loss.clone().detach().mean(),
+            f"{split}/ce_loss": ce_loss.detach().mean(),
+            f"{split}/lovasz_loss": lovasz_loss.detach().mean(),
+            f"{split}/quant_loss": qloss.detach().mean()
+        }
+        return loss, log_dict
+
+    @staticmethod
+    def lovasz_grad(gt_sorted):
+        """
+        # https://github.com/bermanmaxim/LovaszSoftmax/blob/master/pytorch/lovasz_losses.py
+        Computes gradient of the Lovasz extension w.r.t sorted errors
+        See Alg. 1 in paper
+        """
+        p = len(gt_sorted)
+        gts = gt_sorted.sum()
+        intersection = gts - gt_sorted.float().cumsum(0)
+        union = gts + (1 - gt_sorted).float().cumsum(0)
+        jaccard = 1. - intersection / union
+        if p > 1: # cover 1-pixel case
+            jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
+        return jaccard
+    
+    @staticmethod
+    def lovasz_softmax(probas, labels, classes='present', per_image=False, ignore=None):
+        """
+        Multi-class Lovasz-Softmax loss
+          probas: [B, C, H, W] Variable, class probabilities at each prediction (between 0 and 1).
+                  Interpreted as binary (sigmoid) output with outputs of size [B, H, W].
+          labels: [B, H, W] Tensor, ground truth labels (between 0 and C - 1)
+          classes: 'all' for all, 'present' for classes present in labels, or a list of classes to average.
+          per_image: compute the loss per image instead of per batch
+          ignore: void class labels
+        """
+        if per_image:
+            loss = CELovaszSoftmaxWithQuant.mean(CELovaszSoftmaxWithQuant.lovasz_softmax_flat(*CELovaszSoftmaxWithQuant.flatten_probas(prob.unsqueeze(0), lab.unsqueeze(0), ignore), classes=classes)
+                              for prob, lab in zip(probas, labels))
+        else:
+            loss = CELovaszSoftmaxWithQuant.lovasz_softmax_flat(*CELovaszSoftmaxWithQuant.flatten_probas(probas, labels, ignore), classes=classes)
+        return loss
+
+    @staticmethod
+    def lovasz_softmax_flat(probas, labels, classes='present'):
+        """
+        Multi-class Lovasz-Softmax loss
+          probas: [P, C] Variable, class probabilities at each prediction (between 0 and 1)
+          labels: [P] Tensor, ground truth labels (between 0 and C - 1)
+          classes: 'all' for all, 'present' for classes present in labels, or a list of classes to average.
+        """
+        if probas.numel() == 0:
+            # only void pixels, the gradients should be 0
+            return probas * 0.
+        C = probas.size(1)
+        losses = []
+        class_to_sum = list(range(C)) if classes in ['all', 'present'] else classes
+        for c in class_to_sum:
+            fg = (labels == c).float() # foreground for class c
+            if (classes is 'present' and fg.sum() == 0):
+                continue
+            if C == 1:
+                if len(classes) > 1:
+                    raise ValueError('Sigmoid output possible only with 1 class')
+                class_pred = probas[:, 0]
+            else:
+                class_pred = probas[:, c]
+            errors = (Variable(fg) - class_pred).abs()
+            errors_sorted, perm = torch.sort(errors, 0, descending=True)
+            perm = perm.data
+            fg_sorted = fg[perm]
+            losses.append(torch.dot(errors_sorted, Variable(CELovaszSoftmaxWithQuant.lovasz_grad(fg_sorted))))
+        return CELovaszSoftmaxWithQuant.mean(losses)
+    
+    @staticmethod
+    def flatten_probas(probas, labels, ignore=None):
+        """
+        Flattens predictions in the batch
+        """
+        if probas.dim() == 3:
+            # assumes output of a sigmoid layer
+            B, H, W = probas.size()
+            probas = probas.view(B, 1, H, W)
+        B, C, H, W = probas.size()
+        probas = probas.permute(0, 2, 3, 1).contiguous().view(-1, C)  # B * H * W, C = P, C
+        labels = labels.view(-1)
+        if ignore is None:
+            return probas, labels
+        valid = (labels != ignore)
+        vprobas = probas[valid.nonzero().squeeze()]
+        vlabels = labels[valid]
+        return vprobas, vlabels
+
+    @staticmethod
+    def mean(l, ignore_nan=False, empty=0):
+        """
+        nanmean compatible with generators.
+        """
+        l = iter(l)
+        if ignore_nan:
+            l = ifilterfalse(CELovaszSoftmaxWithQuant.isnan, l)
+        try:
+            n = 1
+            acc = next(l)
+        except StopIteration:
+            if empty == 'raise':
+                raise ValueError('Empty mean')
+            return empty
+        for n, v in enumerate(l, 2):
+            acc += v
+        if n == 1:
+            return acc
+        return acc / n
+    
+    @staticmethod
+    def isnan(x):
+        return x != x
+
 
 
 class BaseLossWithDiscriminator(nn.Module):
